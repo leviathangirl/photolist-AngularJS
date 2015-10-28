29a78f4071a4d05228e972f426fc43792be53206
catscarlet <i@catscarlet.com>
2015-10-28 23:52:08 +0800

    router on the way

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('PhotoList',[]);
+var app = angular.module('PhotoList', []);
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'MainController'
+    }
+    .when('/photos/:id', {
+    controller: 'PhotoController',
+  templateUrl: 'views/photo.html' }
+
+    )
+  ).otherwise({
+      redirectTo: '/'
+    });
+
+});
